7adcb3f05d426d7cab30a918d0dbd1acefa0bec9
kamils224 <kamil.sagalara@gmail.com>
2021-01-11 15:18:15 +0100

    Add django simple jwt

--- stx_training_program/settings.py
+++ stx_training_program/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,6 +54,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "stx_training_program.urls"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
 
 TEMPLATES = [
     {
