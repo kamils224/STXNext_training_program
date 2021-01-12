df6e1aaff7404d62697f6f3cfff23f403fd1378b
kamils224 <kamil.sagalara@gmail.com>
2021-01-12 13:58:07 +0100

    Fix docstrings in models.py

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.base_user import BaseUserManager
 class EmailUsernameUserManager(BaseUserManager):
     def create_user(self, email, password=None):
         """
-        Creates and saves a User with the given email, and password.
+        Creates and saves a User with the given email and password.
         """
         if not email:
             raise ValueError("Users must have an email address")
@@ -21,7 +21,7 @@ class EmailUsernameUserManager(BaseUserManager):
 
     def create_superuser(self, email, password=None):
         """
-        Creates and saves a superuser with the given email, and password.
+        Creates and saves a superuser with the given email and password.
         """
         user = self.create_user(
             email,
