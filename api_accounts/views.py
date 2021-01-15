1d100a23ec6c5dacd0f8567442b7df841acce321
kamils224 <kamil.sagalara@gmail.com>
2021-01-15 16:48:13 +0100

    Remove unecessary imports

--- api_accounts/views.py
+++ api_accounts/views.py
@@ -2,10 +2,8 @@ from django.contrib.auth import get_user_model
 from django.db import transaction
 from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-
 
 from api_accounts.models import User
 from api_accounts.serializers import UserRegistrationSerializer, UserSerializer, ActivateAccountSerializer
