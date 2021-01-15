f62062eb0bc72595b1746723c9dc95f12cae67bf
kamils224 <kamil.sagalara@gmail.com>
2021-01-15 13:54:04 +0100

    Celery config

--- stx_training_program/__init__.py
+++ stx_training_program/__init__.py
@@ -1,3 +1,5 @@
-from .celery import app as celery_app
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from stx_training_program.celery import app as celery_app
 
 __all__ = ('celery_app',)
