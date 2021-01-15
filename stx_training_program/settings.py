dd3be4ec8ad7d51f738e9cdfb2310c42749f49a2
kamils224 <kamil.sagalara@gmail.com>
2021-01-15 13:02:24 +0100

    Add default permission class

--- stx_training_program/settings.py
+++ stx_training_program/settings.py
@@ -61,7 +61,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
 }
 
 # settings from https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
