4bd70a1015c59ac68140c93f9357b484c309ea02
kamils224 <kamil.sagalara@gmail.com>
2021-01-15 10:39:54 +0100

    Add unit tests template

--- api_projects/tests.py
+++ api_projects/tests.py
@@ -1,6 +1,14 @@
 from rest_framework.test import APITestCase
 
+from api_projects.models import Project
+from api_accounts.models import User
+
 
 # TODO
-class UserAccountTest(APITestCase):
-    pass
+class UserProjectsTest(APITestCase):
+
+    def setUp(self):
+        user = User.objects.create_user(email="project_owner@example.com",
+                                        password="password123")
+        user.is_active = True
+        user.save()
